950378a3ae8b04dc18dbaa277b8203459b4bd48d
lengyu <lengyuchn@qq.com>
2019-01-21 22:13:05 +0800

    add CORS support

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"github.com/ferriciron/GoJudgeWeb/common"
 	"github.com/ferriciron/GoJudgeWeb/routers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 func main() {
 	common.ParseConfig()
 	go common.ConfigFileWatching()
 	r := gin.New()
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins:true,
+		AllowMethods:     []string{"PUT", "PATCH","POST","GET"},
+		AllowHeaders:     []string{"Origin","token"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	routers.SetRouters(r)
